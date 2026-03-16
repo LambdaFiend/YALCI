ca448a027af5db09043ea317378b9e1af433d9ed
Gabriel Carlos <42923229+LambdaFiend@users.noreply.github.com>
2026-03-16 16:00:27 +0000

    Refactoring

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -19,122 +19,107 @@ eval1 t = let tm = getTm t; fi = getFI t in
   case tm of
     TmApp (TermNode _ (TmAbs _ _ t11)) v2 | isVal v2 -> getTm $ evalSubst v2 t11
     TmApp (TermNode _ (TmWildCard _ t11)) v2 | isVal v2 -> getTm t11
-    TmApp v1 t2 | isVal v1 -> let result = eval1 t2 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmApp v1 result
-    TmApp t1 t2 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmApp result t2
+    TmApp v1 t2 | isVal v1 ->
+      let result = eval1 t2
+       in checkError result $ TmApp v1 result
+    TmApp t1 t2 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmApp result t2
     TmIf (TermNode _ TmTrue) t2 t3 -> getTm t2
     TmIf (TermNode _ TmFalse) t2 t3 -> getTm t3
-    TmIf t1 t2 t3 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmIf result t2 t3
-    TmSucc t1 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmSucc result
+    TmIf t1 t2 t3 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmIf result t2 t3
+    TmSucc t1 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmSucc result
     TmPred (TermNode _ TmZero) -> TmZero
     TmPred (TermNode _ (TmSucc nv1)) | isVal nv1 -> getTm nv1
-    TmPred t1 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmPred result
+    TmPred t1 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmPred result
     TmIsZero (TermNode _ TmZero) -> TmTrue
     TmIsZero (TermNode _ (TmSucc nv1)) | isVal nv1 -> TmFalse
-    TmIsZero t1 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmIsZero result
+    TmIsZero t1 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmIsZero result
     TmSeq (TermNode _ TmUnit) t2 -> getTm t2
-    TmSeq t1 t2 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmSeq result t2
-    TmAscribe t1 ty | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmAscribe result ty
+    TmSeq t1 t2 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmSeq result t2
+    TmAscribe t1 ty | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmAscribe result ty
     TmAscribe v1 _ -> getTm v1
-    TmLet p t1 t2 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmLet p result t2
-    TmLet p v1 t2 -> let subs = reverse $ match p v1; result = foldr ($) t2 subs in
-      if elem (TermNode noPos $ TmErr "Matching error") $ map (\x -> x $ TermNode noPos TmTrue) subs
-        then TmErr "Matching error, either not perfect matching slots or non-matching labels"
-        else getTm result
+    TmLet p t1 t2 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $  TmLet p result t2
+    TmLet p v1 t2 ->
+      let subs = reverse $ match p v1; result = foldr ($) t2 subs
+       in if elem (TermNode noPos $ TmErr "Matching error") $ map (\x -> x $ TermNode noPos TmTrue) subs
+            then TmErr "Matching error, either not perfect matching slots or non-matching labels"
+            else getTm result
     TmProj v1@(TermNode _ (TmRecord ts)) x | isVal v1 -> getTm $ fromMaybe $ lookup x ts
-    TmProj t1 x -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmProj result x
+    TmProj t1 x ->
+      let result = eval1 t1
+       in checkError result $ TmProj result x
     TmRecord ts ->
       let firstHalf = takeWhile (\(_, y) -> isVal y) ts
           secondHalf = dropWhile (\(_, y) -> isVal y) ts
           newMiddle = (\ts -> case ts of (t:ts) -> [(fst t, eval1 $ snd t)]; [] -> []) secondHalf
           rest = (\ts -> case ts of (t:ts) -> ts; [] -> []) secondHalf
-       in
-        case newMiddle of
-          [(_, TermNode _ (TmErr e))] -> TmErr e
-          _ -> TmRecord (firstHalf ++ newMiddle ++ rest)
-    TmVariant x t1 ty | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmVariant x result ty
-    TmCase t1 ts1 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmCase result ts1
+       in case newMiddle of
+            [(_, TermNode _ (TmErr e))] -> TmErr e
+            _ -> TmRecord (firstHalf ++ newMiddle ++ rest)
+    TmVariant x t1 ty | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmVariant x result ty
+    TmCase t1 ts1 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmCase result ts1
     TmCase (TermNode _ (TmVariant x v1 ty)) ts -> getTm $ evalSubst v1 $ snd $ fromMaybe $ lookup x ts
     TmFix (TermNode _ (TmAbs x ty t2)) -> getTm $  evalSubst t t2
-    TmFix t1 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmFix result
-    TmCons ty v1 t2 | isVal v1 -> let result = eval1 t2 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmCons ty v1 result
-    TmCons ty t1 t2 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmCons ty result t2
+    TmFix t1 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmFix result
+    TmCons ty v1 t2 | isVal v1 ->
+      let result = eval1 t2
+       in checkError result $ TmCons ty v1 result
+    TmCons ty t1 t2 ->
+      let result = eval1 t1
+       in checkError result $ TmCons ty result t2
     TmIsNil _ (TermNode _ (TmNil _)) -> TmTrue
     TmIsNil _ (TermNode _ (TmCons _ v1 v2)) | isVal v1 && isVal v2 -> TmFalse
-    TmIsNil ty t1 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmIsNil ty $ eval1 t1
+    TmIsNil ty t1 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmIsNil ty $ eval1 t1
     TmHead _ (TermNode _ (TmCons _ v1 v2)) | isVal v1 && isVal v2 -> getTm v1
-    TmHead ty t1 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmHead ty result
+    TmHead ty t1 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmHead ty result
     TmTail _ (TermNode _ (TmCons _ v1 v2)) | isVal v1 && isVal v2 -> getTm v2
-    TmTail ty t1 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmTail ty result
-    TmTyApp t1 ty | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmTyApp result ty
+    TmTail ty t1 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmTail ty result
+    TmTyApp t1 ty | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmTyApp result ty
     TmTyApp (TermNode _ (TmTyAbs _ t11)) ty -> getTm $ evalTyTermSubst ty t11
     TmUnpack _ _ (TermNode _ (TmPack ty11 v11 ty12)) t2 | isVal v11 ->
       getTm $ evalTyTermSubst ty11 (evalSubst (shift' 0 1 v11) t2)
-    TmUnpack x1 x2 t1 t2 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmUnpack x1 x2 result t2
-    TmPack ty1 t1 ty2 | not $ isVal t1 -> let result = eval1 t1 in
-      case getTm result of
-        TmErr e -> TmErr e
-        _ -> TmPack ty1 result ty2
+    TmUnpack x1 x2 t1 t2 | not $ isVal t1 ->
+      let result = eval1 t1
+      in checkError result $ TmUnpack x1 x2 result t2
+    TmPack ty1 t1 ty2 | not $ isVal t1 ->
+      let result = eval1 t1
+       in checkError result $ TmPack ty1 result ty2
     _ -> TmErr $ "No rule applies" ++ showFileInfo fi
+  where
+    checkError :: TermNode -> Term -> Term
+    checkError term result =
+      case getTm term of
+        TmErr e -> TmErr e
+        _ -> result
 
 type Counter = Int
 
